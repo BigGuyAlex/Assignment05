0acc83cd7cb1f8c1d89413cb05c95b11d0009914
BigGuyAlex <84516641+BigGuyAlex@users.noreply.github.com>
2021-06-16 00:19:28 +0000

    freaafewfaf

---
+++ script.js
@@ -0,0 +1,18 @@
+let number = 0
+let area = 0
+let circumference = 0
+let PI = 3.14
+
+document.getElementById('button').addEventListener('click', calculate)
+
+function calculate () {
+  number = document.getElementById('input').value
+  number = parseInt(number)
+  PI = PI
+  area = 0.25 * number ** 2 * PI
+  circumference = PI * number
+  document.getElementById('answer1').innerHTML = 'Area = ' + area
+  document.getElementById('meaning2').innerHTML = 'Therefore, the Area of the Circle is ' + area
+  document.getElementById('answer2').innerHTML = 'Circumference = ' + circumference
+  document.getElementById('meaning1').innerHTML = 'The Circumference of the Circle is ' + circumference
+}
